271371eb3b840a841af085a5c0d5072b53453ee7
catalinbroinas <catalinbroinas@yahoo.com>
2023-12-20 10:05:20 +0200

    Modify verifyInterval() JS function Improved feature to prevent user from entering unwanted data

--- js/style.js
+++ js/style.js
@@ -2,56 +2,52 @@ const minInput = document.querySelector('#min-number');
 const maxInput = document.querySelector('#max-number');
 const applyButton = document.querySelector('#apply-btn');
 
-function getInterval()
-{
+function getInterval() {
     const min = minInput.value;
     const max = maxInput.value;
 
     return [min, max];
 }
 
-function getRandomNumber()
-{
+function getRandomNumber() {
     const interval = getInterval();
     const min = Math.ceil(interval[0]);
     const max = Math.floor(interval[1]);
-    
+
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function displayResult()
-{
+function displayResult() {
     const result = document.querySelector('#result');
 
     result.style.display = "flex";
     result.textContent = getRandomNumber();
 }
 
-function verifyInterval()
-{
+function verifyInterval() {
     const min = minInput.value;
     const max = maxInput.value;
 
-    maxInput.setAttribute('min', parseInt(min) + 1);
+    (parseInt(min) < 990) ? maxInput.setAttribute('placeholder', parseInt(min) + 10)
+        : maxInput.setAttribute('placeholder', 999);
 
-    if(parseInt(min) < 990)
-    {
-        maxInput.setAttribute('placeholder', parseInt(min) + 10);
-    }
-    else
-    {
-        maxInput.setAttribute('placeholder', 999);
-    }
+    (parseInt(min) < 999) ? maxInput.setAttribute('min', parseInt(min) + 1) : maxInput.setAttribute('min', 1);
 
-    if(parseInt(min) >= parseInt(max))
-    {
-        applyButton.setAttribute('disabled', "");
+    if (!(min == '' || max == '') && !(parseInt(min) > 998 || parseInt(max) > 999)) {
+        if (parseInt(max) > parseInt(min)) {
+            if (applyButton.disabled) {
+                applyButton.removeAttribute('disabled');
+            }
+        }
+        else {
+            if (!applyButton.disabled) {
+                applyButton.setAttribute('disabled', "");
+            }
+        }
     }
-    else
-    {
-        if(applyButton.disabled)
-        {
-            applyButton.removeAttribute('disabled');
+    else {
+        if (!applyButton.disabled) {
+            applyButton.setAttribute('disabled', "");
         }
     }
 }
